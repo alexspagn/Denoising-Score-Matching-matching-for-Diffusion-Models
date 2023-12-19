cfdf8163eac3d84645904a051fd4e235f8bcbdc3
alexspagn <139008447+alexspagn@users.noreply.github.com>
2023-12-19 20:24:30 +0100

    Add files via upload

---
+++ compute_FID.py
@@ -0,0 +1,26 @@
+import torch
+from pytorch_fid.fid_score import calculate_fid_given_paths
+import os
+
+def calculate_fid(relative_path1, relative_path2):
+    # Get the absolute paths based on the current working directory
+    base_path = os.getcwd()
+    path1 = os.path.join(base_path, relative_path1)
+    path2 = os.path.join(base_path, relative_path2)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    fid_value = calculate_fid_given_paths([path1, path2],
+                                          batch_size=100,
+                                          device=device,
+                                          dims=2048)
+    return fid_value
+
+if __name__ == '__main__':
+    # Define the relative paths to the image folders
+    relative_path_to_folder1 = 'exp/image_samples/MNIST_geom_samples'
+    relative_path_to_folder2 = 'MNIST_FID/testSet'
+
+    # Calculate FID
+    fid_score = calculate_fid(relative_path_to_folder1, relative_path_to_folder2)
+    print(f"FID score: {fid_score}")
